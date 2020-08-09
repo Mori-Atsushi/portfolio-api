00a426be46e32ceb1626af1146c9a2103329fe1a
Mori Atsushi <at.sushi.tb@gmail.com>
2020-08-09 16:25:40 +0900

    Delete unused code

--- src/main/kotlin/atsushi/work/api/repository/BlogRepository.kt
+++ src/main/kotlin/atsushi/work/api/repository/BlogRepository.kt
@@ -24,29 +24,6 @@ class BlogRepository(
 
     fun getItem(id: Int): BlogArticle? = articleDB.getItem(id)
 
-    fun isExistPrev(
-        limit: Int,
-        offset: Int,
-        categoryIds: List<Int>? = null
-    ): Boolean {
-        val prevArticle =
-            if (offset > 0) {
-                getList(1, offset - 1, categoryIds)
-            } else {
-                emptyList()
-            }
-        return prevArticle.isNotEmpty()
-    }
-
-    fun isExistNext(
-        limit: Int,
-        offset: Int,
-        categoryIds: List<Int>? = null
-    ): Boolean {
-        val nextArticle = getList(1, offset + limit, categoryIds)
-        return nextArticle.isNotEmpty()
-    }
-
     /**
      * ブログが読まれたことを保存する
      *
