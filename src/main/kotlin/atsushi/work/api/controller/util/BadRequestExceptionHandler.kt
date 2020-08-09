4e8d7a86c083715d78c367860d706465ad726e3d
Mori Atsushi <at.sushi.tb@gmail.com>
2020-08-09 11:56:45 +0900

    Fix ExceptionHandler

--- src/main/kotlin/atsushi/work/api/controller/util/BadRequestExceptionHandler.kt
+++ src/main/kotlin/atsushi/work/api/controller/util/BadRequestExceptionHandler.kt
@@ -5,12 +5,11 @@ import atsushi.work.api.model.exception.NotFoundException
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
-import javax.servlet.http.HttpServletRequest
 
 @ControllerAdvice
 class BadRequestExceptionHandler {
     @ExceptionHandler(NotFoundException::class)
-    fun getNotFoundException(req: HttpServletRequest, e: NotFoundException): ResponseEntity<ErrorResponse> {
+    fun getNotFoundException(e: NotFoundException): ResponseEntity<ErrorResponse> {
         return ResponseEntity(ErrorResponse("not found"), HttpStatus.NOT_FOUND)
     }
 }
